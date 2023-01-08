7381609a93ab1cc8a3b5c5959c2c9b6b1d17828b
yogendra <yogendrakarimi@gmail.com>
2023-01-08 16:22:49 +0530

    Initial Commit

---
+++ src/main/java/com/example/gitproject/DevClass.java
@@ -0,0 +1,7 @@
+package com.example.gitproject;
+
+public class DevClass {
+    public static void main(String args[]){
+        System.out.println("Dev Class");
+    }
+}
---
+++ src/test/java/com/example/gitproject/TestClass.java
@@ -0,0 +1,8 @@
+package com.example.gitproject;
+
+public class TestClass {
+
+    public static void main(String args[]){
+        System.out.println("Test Class");
+    }
+}
