5a254b06b8f27db390a038da9a03a5f0c3952397
yogendra <yogendrakarimi@gmail.com>
2023-01-08 16:56:12 +0530

    Initial Commit

--- src/main/java/com/example/gitproject/DevClass.java
+++ src/main/java/com/example/gitproject/DevClass.java
@@ -1,7 +1,8 @@
 package com.example.gitproject;
 
 public class DevClass {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Dev Class");
+        System.out.println("Dev Class Second Commit");
     }
 }
