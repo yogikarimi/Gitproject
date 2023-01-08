eda1f4b186c6d90361d7b756cab6ef2bc1f6d0e1
yogikarimi <36192880+yogikarimi@users.noreply.github.com>
2023-01-08 17:17:55 +0530

    Update DevClass.java

--- src/main/java/com/example/gitproject/DevClass.java
+++ src/main/java/com/example/gitproject/DevClass.java
@@ -4,5 +4,6 @@ public class DevClass {
     public static void main(String[] args){
         System.out.println("Dev Class");
         System.out.println("Dev Class Second Commit");
+         System.out.println("Dev Class from Yogi Branch git");
     }
 }
